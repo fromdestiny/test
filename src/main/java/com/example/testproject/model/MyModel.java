5eeb470be951217f6c27939d2ea4ffda6e1abaa8
Buxi <>
2019-01-17 12:22:04 +0900

    first commit

---
+++ src/main/java/com/example/testproject/model/MyModel.java
@@ -0,0 +1,23 @@
+package com.example.testproject.model;
+
+public class MyModel {
+  private String name;
+
+  private int phonenumber;
+
+  public int getPhonenumber() {
+    return phonenumber;
+  }
+
+  public void setPhonenumber(int phonenumber) {
+    this.phonenumber = phonenumber;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/com/example/testproject/protocol/Address.java
@@ -0,0 +1,23 @@
+package com.example.testproject.protocol;
+
+public class Address {
+  private String add;
+
+  private long location;
+
+  public String getAdd() {
+    return add;
+  }
+
+  public void setAdd(String add) {
+    this.add = add;
+  }
+
+  public long getLocation() {
+    return location;
+  }
+
+  public void setLocation(long location) {
+    this.location = location;
+  }
+}
